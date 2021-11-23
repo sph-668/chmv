93cffcf9a929c9d371b3b6f217f193ac30b29ffb
sph-668 <sophiasmile48@gmail.com>
2021-11-23 11:33:12 -0500

    initial

---
+++ timetable/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+class Mytimetable(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    teacher = models.TextField()
+    group = models.TextField()
+    lesson = models.TextField()
+    cabinet = models.IntegerField()
+    age = models.NOT_PROVIDED()
+
+    def show(self):
+        pass
+
+
+# Create your models here.

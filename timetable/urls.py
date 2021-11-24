15ebd5c22e40b3150ff10dbdd7b067bf410a2fcf
sph-668 <sophiasmile48@gmail.com>
2021-11-24 02:42:13 -0500

    links working (half)

--- timetable/urls.py
+++ timetable/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.index, name='index'),
+    path('search_date/', views.data_for_user, name='data_for_user'),
+    path('search_more/', views.lessons_for_user, name='lessons_for_user'),
 ]
--- timetable/views.py
+++ timetable/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Mytimetable
 
-def post_list(request):
-    posts = Mytimetable.objects.order_by('group')
-    return render(request, 'timetable/index.html', {'posts': posts})
+def index(request):
+    return render(request, 'timetable/index.html')
+
+def data_for_user(request):
+    return render(request, 'timetable/data_for_user.html')
+
+def lessons_for_user(request):
+    return render(request, 'timetable/lessons_for_user.html')
